36672d7d25a12a03861ea85c36e8e4eb5a40b4ed
Kirainc <164600044+Kirainc@users.noreply.github.com>
2024-11-23 03:06:26 -0800

    Create Treasure_Kaitun.lua

---
+++ Treasure_Kaitun.lua
@@ -0,0 +1,19 @@
+_G.KaitunConfig = {
+    ["Start Farm"] = true,
+
+	  ["Select"] = "Legendary Chest",
+
+    ["Buy Item"] = true,
+	  ["Buy Tool"] = true,
+
+    -- [Setting]
+    ["Disable 3D Rendering"] = true,
+    ["Low Quality Graphic"] = true,
+    ["Kaitun Screen"] = "1", -- "1", "Many",
+    
+    ["Away From Keyboard Checking"] = true,
+    ["Automatically Rejoin"] = true,
+
+    -- [lock]
+    ["Amount"] = 100000,
+}
